9aa27af2d8341564c2b73cacb919fb2f37318176
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-01-26 15:47:36 +0100

    Initial runner setup

--- src/OrderRulesEngine.Runner/Program.cs
+++ src/OrderRulesEngine.Runner/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderRulesEngine.DataAccess.Membership;
+using OrderRulesEngine.Rules;
+using System;
 
-namespace OrderRulesEngine.Runner
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Testing OrderRulesEngine...");
+ServiceCollection serviceCollection = new();
+
+serviceCollection.AddScoped<IMembershipRepository, MembershipRepository>();
+serviceCollection.AddScoped<IOrderRule, AgentCommission>();
+serviceCollection.AddScoped<IOrderRule, LearningToSki>();
+serviceCollection.AddScoped<IOrderRule, MembershipEmail>();
+serviceCollection.AddScoped<IOrderRule, MembershipNew>();
+serviceCollection.AddScoped<IOrderRule, MembershipUpgrade>();
+serviceCollection.AddScoped<IOrderRule, PackingSlipRoyalties>();
+serviceCollection.AddScoped<IOrderRule, PackingSlipShipping>();
+serviceCollection.AddSingleton<Runner>();
+
+serviceCollection
+    .BuildServiceProvider()
+    .GetRequiredService<Runner>()
+    .Run();
+
+Console.ReadLine();
---
+++ src/OrderRulesEngine.Runner/Runner.cs
@@ -0,0 +1,19 @@
+﻿using OrderRulesEngine.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Runner
+{
+    private readonly IEnumerable<IOrderRule> _rules;
+        
+    public Runner(IEnumerable<IOrderRule> rules)
+    {
+        _rules = rules;
+    }
+
+    public void Run()
+    {
+        Console.WriteLine(_rules.Count());
+    }
+}
