0f7b7c6ef7c98f91d2b03551d230abe2623e0eea
Rasmus Edvardsen <Rasmus.Edvardsen@infomedia.dk>
2021-01-26 14:32:51 +0100

    Add dumb data access

---
+++ src/OrderRulesEngine/DataAccess/Membership/IMembershipRepository.cs
@@ -0,0 +1,16 @@
+﻿using OrderRulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderRulesEngine.DataAccess.Membership
+{
+    public interface IMembershipRepository
+    {
+        void ActivateMembership(MembershipProduct product);
+        void UpgradeMembership(MembershipProduct product);
+        void EmailCustomer(Customer customer);
+    }
+}
---
+++ src/OrderRulesEngine/DataAccess/Membership/MembershipRepository.cs
@@ -0,0 +1,27 @@
+﻿using OrderRulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderRulesEngine.DataAccess.Membership
+{
+    public class MembershipRepository : IMembershipRepository
+    {
+        public void ActivateMembership(MembershipProduct product)
+        {
+            // implement logic
+        }
+
+        public void EmailCustomer(Customer customer)
+        {
+            // implement logic
+        }
+
+        public void UpgradeMembership(MembershipProduct product)
+        {
+            // implement logic
+        }
+    }
+}
